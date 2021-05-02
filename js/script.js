71b9679084e47df70d422e6ccfba5dcb6d134659
spiritedwolf <sufismoker31@gmail.com>
2021-05-02 14:36:39 +0530

    array created for the products items

---
+++ js/script.js
@@ -0,0 +1,113 @@
+const productsAr = [
+    {
+        id: 1,
+        image: `brazil-single-origin-coffee.png`,
+        rating: 4,
+        name: `Brazil Single Origin`,
+        orignalPrice: 40,
+        salePrice:  35,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `It has a nutty and full bodied taste, which is incredible as a single origin and a staple for most blends. There complete focus on quality, high scoring, incomparable coffees really shows in the flavour.`
+    },
+    {
+        id: 2,
+        image: `peru-single-origin-coffee.png`,
+        rating: 4,
+        name: `Peru Single Origin`,
+        orignalPrice: 55,
+        salePrice:  25,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `As an origin, Peru has all the conditions necessary to produce world-class coffee: Its high elevation; prominence of good varieties like Typica, Bourbon, and Caturra. The small farm holders and cooperatives are operating in a quality focused movement especially its organic practises and farming techniques.`
+    },
+    {
+        id: 3,
+        image: `blend-coffee.png`,
+        rating: 5,
+        name: `Escapada Blend`,
+        orignalPrice: 20,
+        salePrice:  18,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 4,
+        description: `Made for the adventures!! Whether you are hiking up a mountain, early morning fishing on the river or exploring new lands, adventurers should drink great coffee. So begin your escapada right with a coffee made for you!`
+    },
+    {
+        id: 4,
+        image: `one-tree-blend-coffee.png`,
+        rating: 4.4,
+        name: `One Tree Blend`,
+        orignalPrice: 50,
+        salePrice:  48,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `One For One! We have partnered with one tree planted, an organisation on a mission to tackle reforestation at all levels and have planted over 4 million trees and counting so far.`
+    },
+    {
+        id: 5,
+        image: `moana-coffee.png`,
+        rating: 4.8,
+        name: `MOANA Speciality Blend`,
+        orignalPrice: 75,
+        salePrice:  59,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `The Ocean couldn't express this enough, she is beautiful, mysterious, awe inspiring and free and we hope to encompass this spirit and deliver it through this amazing specialty blend MOANA.`
+    },
+    {
+        id: 6,
+        image: `brazil-single-origin-coffee.png`,
+        rating: 4,
+        name: `Brazil Single Origin`,
+        orignalPrice: 40,
+        salePrice:  35,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `It has a nutty and full bodied taste, which is incredible as a single origin and a staple for most blends. There complete focus on quality, high scoring, incomparable coffees really shows in the flavour.`
+    },
+    {
+        id: 7,
+        image: `peru-single-origin-coffee.png`,
+        rating: 4,
+        name: `Peru Single Origin`,
+        orignalPrice: 55,
+        salePrice:  25,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `As an origin, Peru has all the conditions necessary to produce world-class coffee: Its high elevation; prominence of good varieties like Typica, Bourbon, and Caturra. The small farm holders and cooperatives are operating in a quality focused movement especially its organic practises and farming techniques.`
+    },
+    {
+        id: 8,
+        image: `blend-coffee.png`,
+        rating: 5,
+        name: `Escapada Blend`,
+        orignalPrice: 20,
+        salePrice:  18,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 4,
+        description: `Made for the adventures!! Whether you are hiking up a mountain, early morning fishing on the river or exploring new lands, adventurers should drink great coffee. So begin your escapada right with a coffee made for you!`
+    },
+    {
+        id: 9,
+        image: `one-tree-blend-coffee.png`,
+        rating: 4.4,
+        name: `One Tree Blend`,
+        orignalPrice: 50,
+        salePrice:  48,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `One For One! We have partnered with one tree planted, an organisation on a mission to tackle reforestation at all levels and have planted over 4 million trees and counting so far.`
+    },
+    {
+        id: 10,
+        image: `moana-coffee.png`,
+        rating: 4.8,
+        name: `MOANA Speciality Blend`,
+        orignalPrice: 75,
+        salePrice:  59,
+        size: [`Small`, `Medium`, `Large`],
+        roast: 5,
+        description: `The Ocean couldn't express this enough, she is beautiful, mysterious, awe inspiring and free and we hope to encompass this spirit and deliver it through this amazing specialty blend MOANA.`
+    }
+];
+
